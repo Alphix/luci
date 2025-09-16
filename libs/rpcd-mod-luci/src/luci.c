d6d757ca3ade0b956532380dc573668c24e7a690
David Härdeman <david@hardeman.nu>
2025-09-16 18:27:56 +0200

    hack

--- libs/rpcd-mod-luci/src/luci.c
+++ libs/rpcd-mod-luci/src/luci.c
@@ -355,6 +355,7 @@ struct lease_entry {
 	struct ether_addr mac;
 	char *hostname;
 	char *duid;
+	char *iaid;
 	union {
 		struct in_addr in;
 		struct in6_addr in6;
@@ -487,13 +488,11 @@ lease_next(void)
 				strtok(NULL, " \t\n"); /* iface */
 
 				e.duid = strtok(NULL, " \t\n"); /* duid */
-
 				if (!e.duid)
 					continue;
 
-				p = strtok(NULL, " \t\n"); /* iaid */
-
-				if (!p)
+				e.iaid = strtok(NULL, " \t\n"); /* iaid */
+				if (!e.iaid)
 					continue;
 
 				if (!strcmp(p, "ipv4")) {
@@ -597,6 +596,7 @@ lease_next(void)
 
 				e.hostname = strtok(NULL, " \t\n");
 				e.duid     = strtok(NULL, " \t\n");
+				e.iaid     = NULL; 
 
 				if (!e.hostname || !e.duid)
 					continue;
@@ -1963,6 +1963,9 @@ rpc_luci_get_dhcp_leases(struct ubus_context *ctx, struct ubus_object *obj,
 
 			if (lease->duid)
 				blobmsg_add_string(&blob, "duid", lease->duid);
+
+			if (lease->iaid)
+				blobmsg_add_string(&blob, "iaid", lease->iaid);
 
 			inet_ntop(lease->af, &lease->addr[0].in6, s, sizeof(s));
 			blobmsg_add_string(&blob, (af == AF_INET) ? "ipaddr" : "ip6addr", s);
